e88614b0299d0436c187a81ec52c02595fb3650f
AlanDReeves <alandanielreeves@gmail.com>
2025-04-22 09:48:12 -1000

    Finished sign-in test
    Completed sign-in page test and sign-in.json

---
+++ tests/sign-in.spec.ts
@@ -0,0 +1,16 @@
+/* eslint-disable max-len */
+import { test, expect } from '@playwright/test';
+
+test.use({
+  storageState: 'sign-in.json',
+});
+
+test('test', async ({ page }) => {
+  await page.goto('http://localhost:3000/auth/signin');
+  await page.locator('input[name="email"]').click();
+  await page.locator('input[name="email"]').fill('john@foo.com');
+  await page.locator('input[name="email"]').press('Tab');
+  await page.locator('input[name="password"]').fill('changeme');
+  await page.getByRole('button', { name: 'Sign in' }).click();
+  await expect(page.getByText('AboutAt the Warrior Rec')).toBeVisible();
+});
