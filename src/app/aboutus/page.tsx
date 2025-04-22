01413ea1c732b081147f0ec7905aca0c3549b5de
simonwlin <slin4@hawaii.edu>
2025-04-22 09:35:37 -1000

    Update About Us page.tsx

--- src/app/aboutus/page.tsx
+++ src/app/aboutus/page.tsx
@@ -18,38 +18,33 @@ const AboutUs = () => (
       <Row className="d-flex justify-content-center text-center mt-4" style={{ display: 'flex' }}>
         <div style={{ flex: 1 }}>
           <h3>Alan Reeves</h3>
-          <p>hi</p>
-          <p>Major: </p>
-          <p>Contact: </p>
-          <p>Github: </p>
+          <p>Major: Computer Science</p>
+          <p>Contact: alandr@hawaii.edu</p>
+          <p>Github: https://github.com/AlanDReeves </p>
         </div>
         <div style={{ flex: 1 }}>
           <h3>Anthony Nguyen</h3>
-          <p>hi</p>
-          <p>Major: </p>
-          <p>Contact: </p>
-          <p>Github: </p>
+          <p>Major: Computer Science</p>
+          <p>Contact: an34</p>
+          <p>Github: https://github.com/anthonyn1023</p>
         </div>
         <div style={{ flex: 1 }}>
           <h3>Simon Lin</h3>
-          <p>hi</p>
-          <p>Major: </p>
-          <p>Contact: </p>
-          <p>Github: </p>
+          <p>Major: Computer Science</p>
+          <p>Contact: slin4@hawaii.edu</p>
+          <p>Github: https://github.com/simonwlin</p>
         </div>
         <div style={{ flex: 1 }}>
           <h3>Angelo Rosal</h3>
-          <p>hi</p>
-          <p>Major: </p>
-          <p>Contact: </p>
-          <p>Github: </p>
+          <p>Major: Computer Science</p>
+          <p>Contact: arosal@hawaii.edu</p>
+          <p>Github: https://github.com/Angelo-Rosal</p>
         </div>
         <div style={{ flex: 1 }}>
           <h3>Maya Buchanan</h3>
-          <p>hi</p>
-          <p>Major: </p>
-          <p>Contact: </p>
-          <p>Github: </p>
+          <p>Major: Computer Science</p>
+          <p>Contact: maya26@hawaii.edu</p>
+          <p>Github: https://github.com/mayabuchanan27</p>
         </div>
       </Row>
     </Container>
