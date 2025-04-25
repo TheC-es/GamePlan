c67958f5973ac79b93de77215286903f6ba75963
AlanDReeves <alandanielreeves@gmail.com>
2025-04-24 20:03:31 -1000

    Update sign-in.spec.ts
    Added some debugging info

--- tests/sign-in.spec.ts
+++ tests/sign-in.spec.ts
@@ -11,7 +11,8 @@ test('test', async ({ page }) => {
   await page.locator('input[name="email"]').fill('john@foo.com');
   await page.locator('input[name="email"]').press('Tab');
   await page.locator('input[name="password"]').fill('changeme');
+  console.log('Before clicking sign in - Current URL:', page.url());
   await page.getByRole('button', { name: 'Sign in' }).click();
-  console.log('Current URL:', page.url());
+  console.log('After clicking, Current URL:', page.url());
   await expect(page.getByText('AboutAt the Warrior Rec')).toBeVisible();
 });
