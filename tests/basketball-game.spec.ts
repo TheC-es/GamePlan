82e90be9670356b3bfa11722bff215300771963e
AlanDReeves <alandanielreeves@gmail.com>
2025-05-06 10:07:47 -1000

    typo removed

--- tests/basketball-game.spec.ts
+++ tests/basketball-game.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
-  await page.goto('http://localhost:3000/volleyball');
+  await page.goto('http://localhost:3000/basketball');
   await expect(page.getByText('Court 1TimeTeam 1Score 1Team')).toBeVisible();
   await expect(page.getByText('Court 2TimeTeam 1Score 1Team')).toBeVisible();
   await expect(page.locator('h1')).toContainText('Basketball Games for');
